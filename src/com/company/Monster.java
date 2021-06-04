8effec9cb2beea1dc2f002a69f936b62bd041fed
Eldar <eldarzr@post.bgu.ac.il>
2021-06-04 15:46:12 +0300

    add enemy

---
+++ src/com/company/Monster.java
@@ -0,0 +1,35 @@
+package com.company;
+
+public class Monster extends Enemy {
+
+    int visionRange;
+
+    public Monster(char tile, String name, int attackPoints, int defensePoints, Health health, int experienceValue, int visionRange) {
+        super(tile, name, attackPoints, defensePoints, health, experienceValue);
+        this.visionRange = visionRange;
+    }
+
+    public void move(){
+
+    }
+
+    @Override
+    public void Combat() {
+
+    }
+
+    @Override
+    public void onKill() {
+
+    }
+
+    @Override
+    public void processStep() {
+
+    }
+
+    @Override
+    public void onDeath() {
+
+    }
+}
